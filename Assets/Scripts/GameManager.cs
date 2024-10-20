9073cbe6e7a8902b9e79961abba798c70beee066
Rui <xurui127@gmail.com>
2024-10-20 18:31:16 +0800

    Upload prototype

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,263 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Difficulty
+{
+    Easy = 0,
+    Medium = 1,
+    Hard = 2,
+}
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Difficulty difficulty;
+    [Header("Prefabs")]
+    [SerializeField] private GameObject numberContainer;
+    [SerializeField] private GameObject OperationContainer;
+    [SerializeField] private GameObject numberPrefab;
+    [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private GameObject OperationPrefab;
+    [SerializeField] private TMP_Text explainText;
+    [SerializeField] int decimalPlaces = 3;
+    [SerializeField] private string result;
+    [SerializeField] private TMP_Text currentOperation;
+
+    [SerializeField] GameObject NextButton;
+
+    private float number1;
+    private float number2;
+    private string formattedNumber1;
+    private string formattedNumber2;
+
+    [SerializeField] private int level;
+
+    Dictionary<string, bool> optionsDictionary = new();
+    List<string> availableOptions = new();
+    private void Awake()
+    {
+        optionsDictionary.Add("<", false);
+        optionsDictionary.Add(">", false);
+        optionsDictionary.Add("=", false);
+
+        availableOptions = new List<string>(optionsDictionary.Keys);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GenerateQuestion();
+        }
+    }
+
+    public void GenerateQuestion()
+    {
+        GenerateNumbers(ref number1, ref formattedNumber1);
+
+        if (level == (int)Difficulty.Easy)
+        {
+            Debug.Log("In E");
+            GenerateEasyQuestion();
+        }
+        else if (level == (int)Difficulty.Medium)
+        {
+            Debug.Log("In M");
+
+            GenerateMediumQuestion();
+        }
+        else
+        {
+            Debug.Log("In H");
+
+            GenerateHardQuestion();
+        }
+
+
+        InitNumbers();
+        InitOptions();
+        GetResult();
+    }
+
+    private void GenerateEasyQuestion()
+    {
+        GenerateNumbers(ref number2, ref formattedNumber2);
+    }
+    private void GenerateMediumQuestion()
+    {
+        bool generateSame = Random.Range(0, 2) == 0;
+        if (generateSame)
+        {
+            number2 = number1;
+            formattedNumber2 = formattedNumber1;
+        }
+        else
+        {
+            GenerateNumbers(ref number2, ref formattedNumber2);
+        }
+    }
+    private void GenerateHardQuestion()
+    { 
+        float minOffset = Mathf.Pow(10, -decimalPlaces) * 25; 
+        float maxOffset = Mathf.Pow(10, -decimalPlaces) * 50; 
+
+        float newNumber;
+
+        do
+        {
+            float offset = Random.Range(minOffset, maxOffset);
+            newNumber = number1 + (Random.Range(0, 2) == 0 ? offset : -offset);
+        }
+        while (newNumber >= 10 || newNumber < 0);
+
+
+        string formatSpecifier = "0." + new string('0', decimalPlaces);
+        string formattedString = newNumber.ToString(formatSpecifier);
+
+        if (formattedString.EndsWith("0"))
+        {
+            int newDigit = Random.Range(1, 10);
+            formattedString = formattedString.Substring(0, formattedString.Length - 1) + newDigit;
+        }
+
+        Debug.Log($"Generated number2: {newNumber}, formatted: {formattedString}");
+
+        number2 = float.Parse(formattedString);
+        formattedNumber2 = formattedString;
+    }
+    private void GenerateNumbers(ref float number, ref string numberToString)
+    {
+        number = 0;
+
+        float integerPart = Random.Range(0, 10);
+        float decimalPart = Mathf.Round(
+                                       Random.Range(0f, 1f) *
+                                       Mathf.Pow(10, decimalPlaces)) /
+                                       Mathf.Pow(10, decimalPlaces);
+
+        float randomNumber = integerPart + decimalPart;
+
+        string formatSpecifier = "0." + new string('0', decimalPlaces);
+        string formattedNumber = randomNumber.ToString(formatSpecifier);
+
+        if (formattedNumber.EndsWith("0"))
+        {
+            int newDigit = Random.Range(1, 10);
+            formattedNumber = formattedNumber.Substring(0, formattedNumber.Length - 1) + newDigit;
+        }
+
+        number = float.Parse(formattedNumber);
+        numberToString = formattedNumber;
+    }
+    private void GetResult()
+    {
+        result = "";
+        if (number1 > number2)
+        {
+            result = ">";
+        }
+        else if (number1 < number2)
+        {
+            result = "<";
+        }
+        else
+        {
+            result = "=";
+        }
+    }
+    private void InitNumbers()
+    {
+        foreach (Transform child in numberContainer.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        explainText.transform.parent.gameObject.SetActive(false);
+
+        NextButton.transform.gameObject.SetActive(false);
+
+        availableOptions = new List<string>(optionsDictionary.Keys);
+
+        var numPerfab1 = Instantiate(numberPrefab, numberContainer.transform);
+        numPerfab1.GetComponent<TextReplacer>().text.text = formattedNumber1;
+
+        var oprationPerfab = Instantiate(OperationPrefab, numberContainer.transform);
+        currentOperation = oprationPerfab.GetComponent<TextReplacer>().text;
+        currentOperation.text = "?";
+
+        var numPerfab2 = Instantiate(numberPrefab, numberContainer.transform);
+        numPerfab2.GetComponent<TextReplacer>().text.text = formattedNumber2;
+    }
+    private void InitOptions()
+    {
+        foreach (Transform child in OperationContainer.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        while (availableOptions.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableOptions.Count);
+            string randomOption = availableOptions[randomIndex];
+            availableOptions.RemoveAt(randomIndex);
+
+            var operation = Instantiate(OperationPrefab, OperationContainer.transform);
+            operation.GetComponent<TextReplacer>().text.text = randomOption;
+            var button = operation.GetComponent<Button>();
+            string optionToCheck = randomOption;
+            var buttonTextComponent = operation.GetComponent<TextReplacer>().text;
+            button.onClick.AddListener(() =>
+            {
+                if (optionToCheck.Equals(result))
+                {
+                    AnswerCorrect(buttonTextComponent);
+                }
+                else
+                {
+                    AnswerIncorrect(buttonTextComponent);
+                }
+            });
+        }
+    }
+    private void AnswerCorrect(TMP_Text text)
+    {
+        foreach (Transform child in OperationContainer.transform)
+        {
+            child.GetComponent<Button>().interactable = false;
+        }
+        text.color = Color.green;
+        explainText.transform.parent.gameObject.SetActive(true);
+
+        NextButton.transform.gameObject.SetActive(true);
+
+        currentOperation.text = result;
+        currentOperation.color = Color.green;
+
+        explainText.text = $"Correct! \n {number1} {result} {number2}";
+
+        level++;
+    }
+
+    private void AnswerIncorrect(TMP_Text text)
+    {
+        foreach (Transform child in OperationContainer.transform)
+        {
+            child.GetComponent<Button>().interactable = false;
+        }
+        text.color = Color.red;
+        explainText.transform.parent.gameObject.SetActive(true);
+
+        NextButton.transform.gameObject.SetActive(true);
+
+        currentOperation.text = result;
+        currentOperation.color = Color.green;
+
+        explainText.text = $"Incorrect! \n" +
+                           $"The correct answer is : {number1} {result} {number2}";
+    }
+}
---
+++ Assets/Scripts/TextReplacer.cs
@@ -0,0 +1,7 @@
+using TMPro;
+using UnityEngine;
+public class TextReplacer : MonoBehaviour
+{
+    [SerializeField] public TMP_Text text;
+
+}
